5bfaafa2b90b3119e787333c275b013eacb3b9b0
Dominic Tarr <dominic.tarr@gmail.com>
2014-02-14 05:18:24 +1300

    learning about parsers

---
+++ parse.js
@@ -0,0 +1,53 @@
+
+
+var pattern = 'versions.*.{version, dependencies: optimist}'
+
+module.exports = parse
+
+function parse (pattern) {
+  var tokens = pattern.split(/([{},.:])/).filter(Boolean)
+
+  function object () {
+    var obj = {}, token
+
+    tokens.shift()
+    do {
+      //should be a name
+      console.log(token)
+      token = tokens.shift().trim()
+      if(/,|}/.test(token)) throw new Error('unexpected '+token)
+      var name = token
+
+      token = tokens.shift().trim()
+      if(/[,}]/.test(token))
+        obj[name] = true
+      else if(':' === token)
+        obj[name] = more()
+      
+      if(tokens[0] === '}') return obj
+    } while (tokens.length && token !== '}')
+    return obj
+  }
+
+  function more () {
+    var path = []
+    while(tokens.length) {
+      var token = tokens[0].trim()
+      switch(token) {
+        case '.': break;
+        case '*': path.push(true); break;
+        case '{': path.push(object()); break;
+        case '}': return path; break;
+        default : path.push(token); break;
+      }
+      tokens.shift()
+    }
+    return path
+  }
+  return more()
+}
+
+
+if(!module.parent)
+  console.log(parse(pattern))
+
